c7b297d06bfbf33f12b563360506eb9f206f5dcb
gballa <geraldballa5@gmail.com>
2021-06-10 12:12:11 +0200

    Initial commit

---
+++ src/config/env.js
@@ -0,0 +1,7 @@
+export const NODE_ENV = process.env.NODE_ENV
+export const NODE_APP_URI = process.env.NODE_APP_URI
+export const PORT = process.env.PORT
+export const MONGO_URI = process.env.MONGO_URI
+export const MONGO_DB = process.env.MONGO_DB
+export const CORS_REGX = process.env.CORS_REGX
+export const BODY_LIMIT = process.env.BODY_LIMIT
